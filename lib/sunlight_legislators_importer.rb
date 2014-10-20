e3e9b06dd82b6b51d28cd3841b678337b9f41bcd
Code Division <codedivision@11th.ventures>
2014-10-20 11:16:42 +0800

    create politician, party, state tables and models

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -16,10 +16,10 @@ end
 # IF YOU WANT TO HAVE THIS FILE RUN ON ITS OWN AND NOT BE IN THE RAKEFILE, UNCOMMENT THE BELOW
 # AND RUN THIS FILE FROM THE COMMAND LINE WITH THE PROPER ARGUMENT.
 # begin
-#   raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
-#   SunlightLegislatorsImporter.import(ARGV[0])
-# rescue ArgumentError => e
-#   $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
-# rescue NotImplementedError => e
-#   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
-# end
+  raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
+  SunlightLegislatorsImporter.import(ARGV[0])
+rescue ArgumentError => e
+  $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
+rescue NotImplementedError => e
+  $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
+end
