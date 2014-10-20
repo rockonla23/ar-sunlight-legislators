e32c379b1c55c77f492064e35308f0816e1b0f6e
Code Division <codedivision@11th.ventures>
2014-10-20 12:21:59 +0800

    seeded

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require 'rake'
 require 'rspec/core/rake_task'
 require_relative 'db/config'
+require_relative 'lib/sunlight_legislators_importer.rb'
 
 
 desc "create the database"
@@ -26,6 +27,12 @@ desc 'Retrieves the current schema version number'
 task "db:version" do
   puts "Current version: #{ActiveRecord::Migrator.current_version}"
 end
+
+desc 'FILL MY DB WITH STUFF!!!!'
+task "db:seed" do
+  SunlightLegislatorsImporter.import(File.dirname(__FILE__) + "/db/data/legislators.csv")
+end
+
 
 desc "Run the specs"
 RSpec::Core::RakeTask.new(:specs)
--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,14 +1,33 @@
 require 'csv'
+require_relative '../app/models/party'
+require_relative '../app/models/politician'
+require_relative '../app/models/state'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
+      newpoliticianhash = {}
       row.each do |field, value|
         # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+        # raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
         # TODO: end
+        case field
+        when "state"
+          if !(State.exists?({name: value}))
+          State.create({name: value})
+          end
+          newpoliticianhash["state_id"] = State.where(name: value).first.id
+        when "party"
+           if !(Party.exists?({name: value}))
+          Party.create({name: value})
+          end
+          newpoliticianhash["party_id"] = Party.where(name: value).first.id
+        else
+          newpoliticianhash[field] = value
+        end
       end
+      Politician.create(newpoliticianhash)
     end
   end
 end
@@ -16,10 +35,10 @@ end
 # IF YOU WANT TO HAVE THIS FILE RUN ON ITS OWN AND NOT BE IN THE RAKEFILE, UNCOMMENT THE BELOW
 # AND RUN THIS FILE FROM THE COMMAND LINE WITH THE PROPER ARGUMENT.
 # begin
-  raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
-  SunlightLegislatorsImporter.import(ARGV[0])
-rescue ArgumentError => e
-  $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
-rescue NotImplementedError => e
-  $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
-end
+#   raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
+#   SunlightLegislatorsImporter.import(ARGV[0])
+# rescue ArgumentError => e
+#   $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
+# rescue NotImplementedError => e
+#   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
+# end
